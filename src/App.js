691647886b2cb744e405999374e8b66e9a5c32f4
Les Ansley <lesansley@protonmail.ch>
2022-11-24 17:19:21 +0200

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import MessagesDisplay from "./components/message-display";
+import allMessages from "./data/messages";
 
 function App() {
+  const messageDisplayRef = React.useRef();
+  const [messages, setMessages] = React.useState(allMessages.slice(0, 8));
+  const addMessage = () =>
+    messages.length < allMessages.length
+      ? setMessages(allMessages.slice(0, messages.length + 1))
+      : null;
+  const removeMessage = () =>
+    messages.length > 0
+      ? setMessages(allMessages.slice(0, messages.length - 1))
+      : null;
+
+  const scrollToTop = () => messageDisplayRef.current.scrollToTop();
+  const scrollToBottom = () => messageDisplayRef.current.scrollToBottom();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxHeight: 200 }} className="messaging-app">
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <button onClick={addMessage}>add message</button>
+        <button onClick={removeMessage}>remove message</button>
+      </div>
+      <hr />
+      <div>
+        <button onClick={scrollToTop}>scroll to top</button>
+      </div>
+      <MessagesDisplay ref={messageDisplayRef} messages={messages} />
+      <div>
+        <button onClick={scrollToBottom}>scroll to bottom</button>
+      </div>
     </div>
   );
 }
